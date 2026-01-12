44fea566c45904b469af4cab605f56314595e8af
Rishabh503 <rishabhtripathi2022@gmail.com>
2026-01-12 16:07:00 +0530

    fixes

--- app/api/admin/videos/[videoId]/route.js
+++ app/api/admin/videos/[videoId]/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import prisma from '../../../../lib/prisma';
 import { requireAdmin } from '../../../../lib/auth';
-import { successResponse, errorResponse, unauthorizedResponse } from '@/lib/apiResponse';
+import { successResponse, errorResponse, unauthorizedResponse } from '../../../../lib/apiResponse';
 
 export async function PATCH(request, { params }) {
   try {
